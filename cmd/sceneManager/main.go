a88cacaad325c472e85ea8d11ad24d636fb10e2f
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-08-23 17:54:35 +0200

    initial request reading

--- cmd/sceneManager/main.go
+++ cmd/sceneManager/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request) {
+		log.Println("Hello world")
+		d, err :=io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Ooooops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+
+	})
+
+	http.ListenAndServe(":9090", nil)
 }
