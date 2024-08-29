86422d6761b098eda46f92acd79251f25f8706c8
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-08-29 18:30:09 +0200

    fixing POST with empty objects

--- internal/handlers/textures.go
+++ internal/handlers/textures.go
@@ -15,47 +15,51 @@ func NewHandler(logger*log.Logger) *Textures {
 	return &Textures{logger}
 }
 
-func (t*Textures) GetTexture(w http.ResponseWriter, r *http.Request) {
+func (t*Textures) GetTexture(rw http.ResponseWriter, r *http.Request) {
 	t.logger.Println("Handle GET request")
 
 	id := r.PathValue("id")
 
 	texture, err := models.GetTexture(id)
 	if err != nil {
-		http.Error(w, "Unable to encode textures data to json", http.StatusInternalServerError)
+		http.Error(rw, "Unable to encode textures data to json", http.StatusInternalServerError)
+		return
 	}
 
-	err = texture.ToJSON(w)
+	err = texture.ToJSON(rw)
 	if err != nil {
-		http.Error(w, "Unable to encode textures data to json", http.StatusInternalServerError)
+		http.Error(rw, "Unable to encode textures data to json", http.StatusInternalServerError)
+		return
 	}
 }
 
-func (t*Textures) GetTextures(w http.ResponseWriter, r *http.Request) {
+func (t*Textures) GetTextures(rw http.ResponseWriter, r *http.Request) {
 	t.logger.Println("Handle GET request")
 
 	texturesList := models.GetTextures()
 
-	err := texturesList.ToJSON(w)
+	err := texturesList.ToJSON(rw)
 	if err != nil {
-		http.Error(w, "Unable to encode textures data to json", http.StatusInternalServerError)
+		http.Error(rw, "Unable to encode textures data to json", http.StatusInternalServerError)
+		return
 	}
 }
 
-func (t*Textures) PostTexture(w http.ResponseWriter, r *http.Request) {
+func (t*Textures) PostTexture(rw http.ResponseWriter, r *http.Request) {
 	t.logger.Println("Handle POST request")
 
 	texture := &models.Texture{}
 
 	err := texture.FromJSON(r.Body)
 	if err != nil {
-		http.Error(w,  err.Error(), http.StatusBadRequest)
+		http.Error(rw,  err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	models.AddTexture(texture)
 }
 
-func (t*Textures) PutTexture(w http.ResponseWriter, r *http.Request) {
+func (t*Textures) PutTexture(rw http.ResponseWriter, r *http.Request) {
 	t.logger.Println("Handle PUT request")
 
 	id := r.PathValue("id")
@@ -64,16 +68,17 @@ func (t*Textures) PutTexture(w http.ResponseWriter, r *http.Request) {
 
 	err := texture.FromJSON(r.Body)
 	if err != nil {
-		http.Error(w,  err.Error(), http.StatusBadRequest)
+		http.Error(rw,  err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	err = models.UpdateTexture(id, texture)
 	if err == models.ErrTextureNotFound {
-		http.Error(w, "Texture not found", http.StatusNotFound)
+		http.Error(rw, "Texture not found", http.StatusNotFound)
 		return
 	}
 	if err != nil {
-		http.Error(w, "Issue occured during search for texture", http.StatusInternalServerError)
+		http.Error(rw, "Issue occured during search for texture", http.StatusInternalServerError)
 		return
 	}
 }
