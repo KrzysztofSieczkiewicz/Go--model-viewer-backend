bcd3cecbf3792fca7e49864bae00d2984fc68347
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-09-24 22:04:21 +0200

    FilesService handler planning

--- FilesService/handlers/files.go
+++ FilesService/handlers/files.go
@@ -17,22 +17,33 @@ func NewFiles(s files.Storage, l *log.Logger) *Files {
 	return &Files{store: s, logger: l}
 }
 
-func (f *Files)PostFile(rw http.ResponseWriter, r *http.Request) {
+// TODO: Add paginated requests handling
+
+// TODO: think about a way of providing filepath and other data (including the file) to the request so a structured 
+// file system can be created
+// preferably, filepath should start with file TYPE (this will be solved separate endpoints for each file type) so each filetype will also have separate storage
+// next - there is no need for nested folders structure as each file has unique id
+// the only question is - do You need a filename to be added to an ID 
+// potentially it increases readability for human readers and further reduces collison chance, but that's not that impactful
+// on the other hand it requires passing additional data with requests (like filename).
+// CURRENTLY - it seems that better solution is to rely purely on ID
+
+// Handles post file request. Doesn't allow for overwriting
+func (f *Files) PostFile(rw http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
-	fn := r.PathValue("filname")
+	fn := r.PathValue("filename")
 
-	// TODO: CREATE A data/file.go to handle file collections (and to allow querying for types, names, etc)
-	// ALSO -> handle paginated requests
+	fp := filepath.Join(id, fn)
 
-	f.saveFile(id, fn, rw, r)
+	f.store.Write(fp, r.Body)
 }
 
+// Handles put file request. Doesn't allow for file creation
+func (f *Files) PutFile(rw http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	fn := r.PathValue("filename")
 
+	fp := filepath.Join(id, fn)
 
-func (f *Files) saveFile(id string, path string, rw http.ResponseWriter, r *http.Request) {
-	fp := filepath.Join(id, path)
-	err := f.store.Write(fp, r.Body)
-	if err != nil {
-		http.Error(rw, "Unable to save file", http.StatusInternalServerError)
-	}
+	f.store.Overwrite(fp, r.Body)
 }
